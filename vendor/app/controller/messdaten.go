7e6cb5e9fca16d1e547700f366faf9751435d8bd
Nicolas Morawietz <nicolas.morawietz@googlemail.com>
2018-06-17 20:33:40 +0200

    +Advanced data structre; +condition in get param

--- vendor/app/controller/messdaten.go
+++ vendor/app/controller/messdaten.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"log"
 	"net/http"
+	"regexp"
 
 	"app/model/feinstaub"
 	"app/shared/response"
@@ -11,51 +12,50 @@ import (
 
 // Routes
 func init() {
-	router.Get("/accelerations", MessdatenAccelGET)
-	router.Get("/particulatematter", ParticulateMatterGET)
+	router.Get("/measurements", MeasurementsGET)
 }
 
 const (
-	ItemCreated      = "item created"
-	ItemExists       = "item already exists"
-	ItemNotFound     = "item not found"
-	ItemFound        = "item found"
-	ItemsFound       = "items found"
-	ItemsFindEmpty   = "no items to find"
-	ItemUpdated      = "item updated"
-	ItemDeleted      = "item deleted"
-	ItemsDeleted     = "items deleted"
-	ItemsDeleteEmpty = "no items to delete"
+	itemsNotFound = "items not found"
+	itemsFound    = "items found"
 
-	FriendlyError = "an error occurred, please try again later"
+	friendlyError = "an error occurred, please try again later"
 )
 
 // *****************************************************************************
 // Read
 // *****************************************************************************
 
-func MessdatenAccelGET(w http.ResponseWriter, r *http.Request) {
+// MeasurementsGET sends measurements json response
+func MeasurementsGET(w http.ResponseWriter, r *http.Request) {
+	// Get condition GET parameter
+	condition := r.URL.Query().Get("condition")
+
+	// Validate condition
+	reg, errReg := regexp.Compile("^(\\w+[<>=]+\\w+(\\s(and|or)\\s?)?)*$")
+	if errReg != nil {
+		log.Println(errReg)
+		response.SendError(w, http.StatusInternalServerError, friendlyError)
+		return
+	}
+	if !reg.MatchString(condition) {
+		log.Println("Wrong condition statement: " + condition)
+		response.SendError(w, http.StatusInternalServerError, itemsNotFound)
+		return
+	}
+
 	// Get all items
-	data, err := feinstaub.ReadAllAccel()
+	data, err := feinstaub.ReadMeasurements(condition)
 	if err != nil {
 		log.Println(err)
-		response.SendError(w, http.StatusInternalServerError, FriendlyError)
+		response.SendError(w, http.StatusInternalServerError, friendlyError)
 		return
 	}
 
 	// Send json data
-	response.Send(w, http.StatusOK, ItemsFound, len(data), data)
-}
-
-func ParticulateMatterGET(w http.ResponseWriter, r *http.Request) {
-	// Get all items
-	data, err := feinstaub.ReadAllParticulate()
-	if err != nil {
-		log.Println(err)
-		response.SendError(w, http.StatusInternalServerError, FriendlyError)
-		return
+	if len(data) == 0 {
+		response.Send(w, http.StatusOK, itemsNotFound, 0, nil)
+	} else {
+		response.Send(w, http.StatusOK, itemsFound, len(data), data)
 	}
-
-	// Send json data
-	response.Send(w, http.StatusOK, ItemsFound, len(data), data)
 }
--- vendor/app/model/feinstaub/feinstaub.go
+++ vendor/app/model/feinstaub/feinstaub.go
@@ -10,19 +10,8 @@ import (
 // Name of the table
 const tableName = "messdaten"
 
-// Acceleration information
-type Acceleration struct {
-	ID            string    `db:"ID" json:"id"`
-	Time          time.Time `db:"DATETIME" json:"datetime" require:"true"`
-	Latitude      string    `db:"LAT" json:"lat" require:"true"`
-	Longitude     string    `db:"LNG" json:"long" require:"true"`
-	AccelerationX float32   `db:"ACCEL_X" json:"accel_x" require:"true"`
-	AccelerationY float32   `db:"ACCEL_Y" json:"accel_y" require:"true"`
-	AccelerationZ float32   `db:"ACCEL_Z" json:"accel_z" require:"true"`
-}
-
-// Acceleration information
-type ParticulateMatter struct {
+// MeasuredData information
+type MeasuredData struct {
 	ID            string    `db:"ID" json:"id"`
 	Time          time.Time `db:"DATETIME" json:"datetime" require:"true"`
 	Latitude      string    `db:"LAT" json:"lat" require:"true"`
@@ -34,51 +23,29 @@ type ParticulateMatter struct {
 	Speed         float32   `db:"SPEED" json:"speed" require:"true"`
 	PM25          float32   `db:"PM25" json:"PM25" require:"true"`
 	PM10          float32   `db:"PM10" json:"PM10" require:"true"`
-	GAS_NH3       float32   `db:"GAS_NH3" json:"GAS_NH3" require:"true"`
-	GAS_CO        float32   `db:"GAS_CO" json:"GAS_CO" require:"true"`
-	GAS_NO2       float32   `db:"GAS_NO2" json:"GAS_NO2" require:"true"`
-	GAS_C3H8      float32   `db:"GAS_C3H8" json:"GAS_C3H8" require:"true"`
-	GAS_C4H10     float32   `db:"GAS_C4H10" json:"GAS_C4H10" require:"true"`
-	GAS_CH4       float32   `db:"GAS_CH4" json:"GAS_CH4" require:"true"`
-	GAS_H2        float32   `db:"GAS_H2" json:"GAS_H2" require:"true"`
-	GAS_C2H5OH    float32   `db:"GAS_C2H5OH" json:"GAS_C2H5OH" require:"true"`
+	GasNH3        float32   `db:"GAS_NH3" json:"GAS_NH3" require:"true"`
+	GasCO         float32   `db:"GAS_CO" json:"GAS_CO" require:"true"`
+	GasNO2        float32   `db:"GAS_NO2" json:"GAS_NO2" require:"true"`
+	GasC3H8       float32   `db:"GAS_C3H8" json:"GAS_C3H8" require:"true"`
+	GasC4H10      float32   `db:"GAS_C4H10" json:"GAS_C4H10" require:"true"`
+	GasCH4        float32   `db:"GAS_CH4" json:"GAS_CH4" require:"true"`
+	GasH2         float32   `db:"GAS_H2" json:"GAS_H2" require:"true"`
+	GasC2H5OH     float32   `db:"GAS_C2H5OH" json:"GAS_C2H5OH" require:"true"`
 }
 
-// AccelData of entities
-type AccelData []Acceleration
-
-// AccelData of entities
-type ParticulateData []ParticulateMatter
-
-// New Acceleration
-func NewAccel() (*Acceleration, error) {
-	var err error
-	Acceleration := &Acceleration{}
-
-	// Set the default parameters
-	Acceleration.ID, err = database.UUID()
-	// If error on UUID generation
-	if err != nil {
-		return Acceleration, err
-	}
-
-	return Acceleration, nil
-}
+// ParticulateData of entities
+type ParticulateData []MeasuredData
 
 // *****************************************************************************
 // Read
 // *****************************************************************************
 
-// ReadAll returns all entities of Acceleration
-func ReadAllAccel() (AccelData, error) {
-	var result AccelData
-	err := database.SQL.Select(&result, fmt.Sprintf("SELECT * FROM %v", tableName))
-	return result, err
-}
-
-// ReadAll returns all entities of ParticulateMatter
-func ReadAllParticulate() (ParticulateData, error) {
+// ReadMeasurements returns all entities of ParticulateMatter
+func ReadMeasurements(condition string) (ParticulateData, error) {
 	var result ParticulateData
-	err := database.SQL.Select(&result, fmt.Sprintf("SELECT * FROM %v", tableName))
+	if len(condition) > 0 {
+		condition = " WHERE " + condition
+	}
+	err := database.SQL.Select(&result, fmt.Sprintf("SELECT * FROM %v%v", tableName, condition))
 	return result, err
 }
